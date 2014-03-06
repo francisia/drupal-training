00136fef01f97a04808d292cc4f2f032fa4ece09
francisia <francis@appnovation.com>
2014-03-06 10:36:59 -0500

    fixed xss issues

--- sites/all/modules/jdt_user_notes/jdt_user_notes_template.tpl.php
+++ sites/all/modules/jdt_user_notes/jdt_user_notes_template.tpl.php
@@ -9,6 +9,6 @@
 <div class='jdt-user-notes-<?php print $note->note_id ?>'>
   <?php print check_plain($note->note_text) ?><br />
   <i>by <?php print check_plain($username) ?></i><br />
-  <a href='../../note/<?php print $note->note_id ?>/edit'>Edit</a>
+  <?php print $edit_link ?>
   <br /><br />
 </div>
