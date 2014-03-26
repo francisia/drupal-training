dacb333cea61c18db88e9796629d753335dace7d
francisia <francis@appnovation.com>
2014-03-26 17:48:13 -0400

    created list of user notes as a view

--- sites/all/modules/jdt_user_notes/jdt_user_notes_template.tpl.php
+++ sites/all/modules/jdt_user_notes/jdt_user_notes_template.tpl.php
@@ -8,7 +8,6 @@
 
 <div class='jdt-user-notes-<?php print $note->note_id ?>'>
   <?php print check_plain($note->note_text) ?><br />
-  <i>by <?php print check_plain($username) ?></i><br />
   <?php print $edit_link ?>
   <br /><br />
 </div>
